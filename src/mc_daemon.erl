18acaf865ad04452518a058385252f0ec61b5396
Dustin Sallings <dustin@spy.net>
2011-04-01 23:05:56 -0700

    Pattern for safely managing DB connections.
    It doesn't currently cover exit/error cases, but is written in such a
    way that can at least handle closing in the normal cases.
    
    Change-Id: I0cd355813ef5f31fe9c89decca12e49863e328d1

--- src/mc_daemon.erl
+++ src/mc_daemon.erl
@@ -22,31 +22,49 @@ start_link() ->
 init([]) ->
     ?LOG_INFO("MC daemon: starting.", []),
     {ok, S} = mc_tcp_listener:start_link(11213, self()),
-    {ok, Db} = couch_db:open(<<"kv">>, []),
-    {ok, #state{mc_serv=S, db=Db}}.
+    DbName = <<"kv">>,
+    {ok, #state{mc_serv=S, db=DbName}}.
+
+with_open_db(F, State) ->
+    {ok, Db} = couch_db:open(State#state.db, []),
+    NewState = F(Db),
+    couch_db:close(Db),
+    NewState.
 
 handle_call({?GET, <<>>, Key, <<>>, _CAS}, _From, State) ->
     error_logger:info_msg("Got GET command for ~p.~n", [Key]),
-    case mc_couch_kv:get(State#state.db, Key) of
-        {ok, Flags, Cas, Data} ->
-            FlagsBin = <<Flags:32>>,
-            {reply,
-             #mc_response{extra=FlagsBin, cas=Cas, body=Data},
-             State};
-        _ ->
-            {reply, #mc_response{status=1, body="Does not exist"}, State}
-    end;
+    with_open_db(fun(Db) ->
+                         case mc_couch_kv:get(Db, Key) of
+                             {ok, Flags, Cas, Data} ->
+                                 FlagsBin = <<Flags:32>>,
+                                 {reply,
+                                  #mc_response{extra=FlagsBin,
+                                               cas=Cas, body=Data},
+                                  State};
+                             _ ->
+                                 {reply, #mc_response{status=1,
+                                                      body="Does not exist"},
+                                  State}
+                         end
+                 end, State);
 handle_call({?SET, <<Flags:32, Expiration:32>>, Key, Value, _CAS},
             _From, State) ->
-    NewCas = mc_couch_kv:set(State#state.db, Key, Flags, Expiration, Value),
-    {reply, #mc_response{cas=NewCas}, State};
+    with_open_db(fun(Db) ->
+                         NewCas = mc_couch_kv:set(Db,
+                                                  Key, Flags,
+                                                  Expiration, Value),
+                         {reply, #mc_response{cas=NewCas}, State}
+                 end, State);
 handle_call({?DELETE, <<>>, Key, <<>>, _CAS}, _From, State) ->
-    case mc_couch_kv:delete(State#state.db, Key) of
-        ok ->
-            {reply, #mc_response{}, State};
-        not_found ->
-            {reply, #mc_response{status=1, body="Not found"}, State}
-    end;
+    with_open_db(fun(Db) ->
+                         case mc_couch_kv:delete(Db, Key) of
+                             ok ->
+                                 {reply, #mc_response{}, State};
+                             not_found ->
+                                 {reply, #mc_response{status=1,
+                                                      body="Not found"}, State}
+                         end
+                 end, State);
 handle_call({_OpCode, _Header, _Key, _Body, _CAS}, _From, State) ->
     {reply, #mc_response{status=?UNKNOWN_COMMAND, body="WTF, mate?"}, State};
 handle_call(Request, _From, State) ->
