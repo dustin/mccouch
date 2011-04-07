5c6f2313014ed9bd46aa5fbea6e9a82da49715c0
Dustin Sallings <dustin@spy.net>
2011-04-07 10:56:40 -0700

    Handle JSON mode vs. non-JSON mode and invalid docs.
    GET must always do the right thing.
    
    Change-Id: Id71603e47b72609183e395f89fa2c5b300b0767e

--- src/mc_couch_kv.erl
+++ src/mc_couch_kv.erl
@@ -2,7 +2,30 @@
 
 -include("couch_db.hrl").
 
--export([get/2, set/5, delete/2]).
+-export([get/2, set/6, delete/2]).
+
+dig_out_attachment(Doc, FileName) ->
+    case [A || A <- Doc#doc.atts, A#att.name == FileName] of
+        [] ->
+            not_found;
+        [Att] ->
+            Segs = couch_doc:att_foldl(Att, fun(Seg, Acc) -> [Seg|Acc] end, []),
+            {ok, iolist_to_binary(lists:reverse(Segs))}
+    end.
+
+cleanup(EJson, []) -> EJson;
+cleanup(EJson, [Hd|Tl]) -> cleanup(proplists:delete(Hd, EJson), Tl).
+cleanup(EJson) ->
+    cleanup(EJson, [<<"_id">>, <<"_rev">>, <<"$flags">>, <<"$expiration">>]).
+
+addRev(Db, Key, ToStore) ->
+    case couch_db:open_doc(Db, Key, []) of
+        {ok, Doc} ->
+            {EJson} = couch_doc:to_json_obj(Doc, []),
+            [{<<"_rev">>, proplists:get_value(<<"_rev">>, EJson)} | ToStore];
+        _ ->
+            ToStore
+    end.
 
 %% ok, Flags, Cas, Data
 -spec get(_, binary()) -> {ok, integer(), integer(), binary()}.
@@ -12,27 +35,67 @@ get(Db, Key) ->
             {EJson} = couch_doc:to_json_obj(Doc, []),
             ?LOG_INFO("Doc keys ~p.", [proplists:get_keys(EJson)]),
             Flags = proplists:get_value(<<"$flags">>, EJson, 0),
-            Encoded = iolist_to_binary(couch_util:json_encode(
-                                         {mc_couch_util:cleanup(EJson)})),
-            ?LOG_INFO("MC daemon: got doc ~p:  ~p.", [Key, Encoded]),
-            {ok, Flags, 0, Encoded};
+
+            case dig_out_attachment(Doc, <<"value">>) of
+                {ok, AttData} ->
+                    {ok, Flags, 0, AttData};
+                _ ->
+                    Encoded = iolist_to_binary(couch_util:json_encode(
+                                                 {cleanup(EJson)})),
+                    ?LOG_INFO("MC daemon: got doc ~p:  ~p.", [Key, Encoded]),
+                    {ok, Flags, 0, Encoded}
+            end;
         _ -> not_found
     end.
 
--spec set(_, binary(), integer(), integer(), binary()) -> integer().
-set(Db, Key, Flags, Expiration, Value) ->
-    {EJson} = couch_util:json_decode(Value),
-    ?LOG_INFO("set ejson with keys: ~p.", [proplists:get_keys(EJson)]),
-    ToStore = [{<<"_id">>, Key},
-               {<<"$flags">>, Flags},
-               {<<"$expiration">>, Expiration}
-               | mc_couch_util:cleanup(EJson)],
+mk_att_doc(Key, Flags, Expiration, Value, Reason) ->
+    [{<<"_id">>, Key},
+     {<<"$flags">>, Flags},
+     {<<"$expiration">>, Expiration},
+     {<<"$att_reason">>, Reason},
+     {<<"_attachments">>,
+      {[{<<"value">>,
+         {[{<<"content_type">>, <<"application/content-stream">>},
+           {<<"data">>, base64:encode(Value)}]}}]}}].
 
-    WithRev = mc_couch_util:addRev(Db, Key, ToStore),
 
+mk_json_doc(Key, Flags, Expiration, Value) ->
+    case (catch couch_util:json_decode(Value)) of
+        {invalid_json, _} ->
+            mk_att_doc(Key, Flags, Expiration, Value, <<"invalid_json">>);
+        {EJson} ->
+            [{<<"_id">>, Key},
+             {<<"$flags">>, Flags},
+             {<<"$expiration">>, Expiration}
+             | cleanup(EJson)]
+    end.
+
+mk_doc(Key, Flags, Expiration, Value, WantJson) ->
+    case WantJson of
+        true ->
+            mk_json_doc(Key, Flags, Expiration, Value);
+        _ ->
+            mk_att_doc(Key, Flags, Expiration, Value, <<"non-JSON mode">>)
+    end.
+
+-spec set(_, binary(), integer(), integer(), binary(), boolean()) -> integer().
+set(Db, Key, Flags, Expiration, Value, JsonMode) ->
+    WithRev = addRev(Db, Key, mk_doc(Key, Flags, Expiration, Value, JsonMode)),
     Doc = couch_doc:from_json_obj({WithRev}),
-
     couch_db:update_doc(Db, Doc, []),
     0.
 
-delete(Db, Key) -> mc_couch_util:delete(Db, Key).
+-spec delete(_, binary()) -> ok|not_found.
+delete(Db, Key) ->
+    case couch_db:open_doc(Db, Key, []) of
+        {ok, Doc} ->
+            {EJson} = couch_doc:to_json_obj(Doc, []),
+            DelMe = [{<<"_deleted">>, true},
+                     {<<"_id">>, Key},
+                     {<<"_rev">>, proplists:get_value(<<"_rev">>, EJson)}],
+            couch_db:update_doc(Db,
+                                couch_doc:from_json_obj({DelMe}), []),
+            ok;
+        _ ->
+            not_found
+    end.
--- src/mc_daemon.erl
+++ src/mc_daemon.erl
@@ -14,7 +14,7 @@
 -include("couch_db.hrl").
 -include("mc_constants.hrl").
 
--record(state, {mc_serv, db}).
+-record(state, {mc_serv, db, json_mode}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -23,7 +23,7 @@ init([]) ->
     ?LOG_INFO("MC daemon: starting.", []),
     {ok, S} = mc_tcp_listener:start_link(11213, self()),
     DbName = <<"kv">>,
-    {ok, #state{mc_serv=S, db=DbName}}.
+    {ok, #state{mc_serv=S, db=DbName, json_mode=false}}.
 
 with_open_db(F, State) ->
     {ok, Db} = couch_db:open(State#state.db, []),
@@ -32,7 +32,7 @@ with_open_db(F, State) ->
     NewState.
 
 handle_get_call(Db, Key) ->
-    case mc_couch_kv_att:get(Db, Key) of
+    case mc_couch_kv:get(Db, Key) of
         {ok, Flags, Cas, Data} ->
             FlagsBin = <<Flags:32>>,
             #mc_response{extra=FlagsBin, cas=Cas, body=Data};
@@ -40,10 +40,11 @@ handle_get_call(Db, Key) ->
             #mc_response{status=1, body="Does not exist"}
     end.
 
-handle_set_call(Db, Key, Flags, Expiration, Value) ->
-    NewCas = mc_couch_kv_att:set(Db,
-                                 Key, Flags,
-                                 Expiration, Value),
+handle_set_call(Db, Key, Flags, Expiration, Value, JsonMode) ->
+    NewCas = mc_couch_kv:set(Db,
+                             Key, Flags,
+                             Expiration, Value,
+                             JsonMode),
     #mc_response{cas=NewCas}.
 
 handle_delete_call(Db, Key) ->
@@ -57,7 +58,9 @@ handle_call({?GET, <<>>, Key, <<>>, _CAS}, _From, State) ->
     with_open_db(fun(Db) -> {reply, handle_get_call(Db, Key), State} end, State);
 handle_call({?SET, <<Flags:32, Expiration:32>>, Key, Value, _CAS},
             _From, State) ->
-    with_open_db(fun(Db) -> {reply, handle_set_call(Db, Key, Flags, Expiration, Value),
+    with_open_db(fun(Db) -> {reply, handle_set_call(Db, Key, Flags,
+                                                    Expiration, Value,
+                                                   State#state.json_mode),
                              State}
                  end, State);
 handle_call({?DELETE, <<>>, Key, <<>>, _CAS}, _From, State) ->
